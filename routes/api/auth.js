068065142aa61339e4f719f593a4b12ae0933e81
Alyona <polova_alyona0907@ukr.net>
2023-12-11 10:45:48 +0200

    delete duplicate logout

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,6 +8,6 @@ router.post("/register", authCtrl.register);
 router.post("/login", authCtrl.login);
 router.get("/current", authenticate, authCtrl.current);
 router.post("/logout", authenticate, authCtrl.logout);
-router.post("/logout", authenticate, authCtrl.logout);
+
 router.patch("/", authenticate, authCtrl.updateSubscription);
 module.exports = router;
